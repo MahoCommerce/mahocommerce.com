f005fcc2d1b5c42dc63783af2317ba13742db26f
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-03-06 17:51:57 +0000

    livereload

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker build -t maho/mkdocs .
 open http://localhost:8000
-docker run --rm -it -p 8000:8000 -v .:/docs maho/mkdocs
+docker run --rm -it -p 8000:8000 -v .:/docs maho/mkdocs serve --dev-addr=0.0.0.0:8000 --livereload
